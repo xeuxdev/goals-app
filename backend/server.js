1f34ed4be0896513616693d17e12543b42593f32
headbwoi <tochukwujohn24@gmail.com>
2022-12-12 10:33:56 +0100

    created routes

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const dotenv = require("dotenv").config()
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use("/api/goals", require("./routes/goalRoutes"))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
