42f79baa62439ae5bf7d56e14f02c4c8de924977
headbwoi <tochukwujohn24@gmail.com>
2022-12-12 11:00:45 +0100

    setup error handler

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const dotenv = require("dotenv").config()
 const port = process.env.PORT || 5000
 const app = express()
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use("/api/goals", require("./routes/goalRoutes"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
